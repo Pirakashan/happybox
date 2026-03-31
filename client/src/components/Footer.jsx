226d75ff10f989e6d875fb0133a3f5e1bed85d88
Pirakashan <mpirakashan@gmail.com>
2026-03-31 17:03:50 +0530

    footer designed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
             <ul>
               <li><Link to="/">Home</Link></li>
               <li><Link to="/events">Events</Link></li>
-              <li><Link to="/about">About</Link></li>
+              <li><Link to="/about">About us </Link></li>
               <li><Link to="/contact">Contact Us</Link></li>
             </ul>
           </div>
